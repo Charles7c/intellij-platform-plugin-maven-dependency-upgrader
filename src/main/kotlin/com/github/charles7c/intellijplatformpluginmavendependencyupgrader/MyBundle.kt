d743a6cf2f596e009a56e53ec82eaa0033289020
GitHub Action <action@github.com>
2026-02-26 01:51:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/charles7c/intellijplatformpluginmavendependencyupgrader/MyBundle.kt
+++ src/main/kotlin/com/github/charles7c/intellijplatformpluginmavendependencyupgrader/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.charles7c.intellijplatformpluginmavendependencyupgrader
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
